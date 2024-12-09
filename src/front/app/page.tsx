710da986ac304e064dc2a5f39edf45b3e5aebdef
Aurélien Audelin <aurelien.audelin@gmail.com>
2024-12-09 18:25:22 +0100

    feat: init daisy template

--- src/front/app/page.tsx
+++ src/front/app/page.tsx
@@ -1,5 +1,8 @@
-import Link from "next/link";
 
 export default function Home() {
-  return <Link href="/goals">Use the App</Link>;
+  return (
+      <>
+        <button className="btn btn-primary">Hello daisyUI!</button>
+      </>
+  )
 }
